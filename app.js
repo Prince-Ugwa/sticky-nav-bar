d49b184a8e3e70e79fa376518863d4f173f4df22
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2023-11-20 22:47:34 +0100

    commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const headerEl = document.querySelector(".header");
-const mainEl = document.querySelector(".show-text");
+const showTextEl = document.querySelector(".show-text");
 
 console.log(headerEl.offsetHeight);
 window.addEventListener("scroll", () => {
-  if (window.scrollY > mainEl.offsetTop - headerEl.offsetHeight - 40) {
+  if (window.scrollY > showTextEl.offsetTop - headerEl.offsetHeight - 40) {
     headerEl.classList.add("active");
   } else headerEl.classList.remove("active");
 });
